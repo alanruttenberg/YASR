ca29dd540c22812b2c4b2a7cca27055771c6484c
Alan Ruttenberg <alanruttenberg@gmail.com>
2015-05-11 16:40:00 -0400

    update datatables

---
+++ gulp/dev.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	paths = require('./paths.js'),
+	livereload = require('gulp-livereload');
+
+gulp.task('watch', function() {
+	gulp.watch(["./src/**/*.js"], [ 'browserifyForDebug' ]);
+	gulp.watch("./src/**/*.scss", [ 'makeCss' ]);
+	  gulp.watch(
+		'./*.html'
+	, function(files) {
+		gulp.src(files.path).pipe(connect.reload());
+	});
+});
+
