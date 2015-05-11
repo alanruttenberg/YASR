76ed2e49f4656912c7e313436bd158694dfd7533
Alan Ruttenberg <alanruttenberg@gmail.com>
2015-05-11 16:48:07 -0400

    support git dev, update jQuery

--- gulpFile.js
+++ gulpFile.js
@@ -3,4 +3,5 @@ require('require-dir')('./gulp');
 var gulp = require('gulp');	
 gulp.task('default', ['modifyYasqeInclude', 'browserify', 'browserifyWithDeps', 'makeCss', 'makeMainPage']);
 gulp.task('serve', ['modifyYasqeIncludeDev', 'makeCss', 'makeMainPage', 'browserifyForDebug', 'watch', 'connect']);
+gulp.task('dev', ['modifyYasqeIncludeDev', 'makeCss', 'makeMainPage', 'browserifyForDebug', 'watch']);
 
